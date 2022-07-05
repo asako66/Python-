7b071b499971f17b058952b0c40bd40b0ad3abc9
asako66 <arketing6429@gmail.com>
2022-07-05 16:16:40 +0900

    Quit

--- main.py
+++ main.py
@@ -50,6 +50,9 @@ def main():
       if event.type == QUIT:
         pygame.quit()
         sys.exit()
+      if event.type == KEYDOWN and event.key == K_ESCAPE:
+        pygame.quit()
+        sys.exit()
 
     # 描画スピードの調整（FPS）
     clock.tick(60)
